28f4af8b221d1e625d0f29d8bf51e0839fcd50ea
Mauricio Oroza Herbas <MauricioOroza@fundacion-jala.local>
2020-02-18 18:02:06 -0400

    Second comit mauricio

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,31 +1,44 @@
+import java.util.Scanner;
+
 public class Main {
 
-    public static void main()
-    {
-        int a=2;
-        int b=3;
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Calcuuuu");
+        int a=0;
+        int b=0;
         int resp=0;
-        int caso=1;
+        int caso=0;
+
+        System.out.println("Ingrese valor a:");
+        a=input.nextInt();
+        System.out.println("Ingrese valor b:");
+        b=input.nextInt();
+        System.out.println("Sumar:1  Restar:2  Multiplicar:3  Dividir:4");
+        caso=input.nextInt();
 
         switch (caso){
             case 1:
                 resp=sumar(a,b);
-                System.out.print(resp);
+                imprimir(resp);
                 break;
             case 2:
                 resp=restar(a,b);
-                System.out.print(resp);
+                imprimir(resp);
                 break;
             case 3:
                 resp=multiplicar(a,b);
-                System.out.print(resp);
+                imprimir(resp);
                 break;
             case 4:
                 resp=dividir(a,b);
-                System.out.print(resp);
+                imprimir(resp);
                 break;
         }
     }
+
+
 
     public static int sumar(int a,int b)
     {
@@ -54,4 +67,10 @@ public class Main {
         return(resp);
     }
 
+    public static void imprimir(int r)
+    {
+        System.out.print("El resultado es: ");
+        System.out.println(r);
+        System.out.println("!!!");
+    }
 }
