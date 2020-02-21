d16b0464a91f9692bf2dc9ef535d95a9a5a23ef6
Carlos Meneses <kenshinmc23@gmail.com>
2020-02-21 19:39:27 -0400

    adding unit test to operations class

--- src/test/java/Carlos/OperacionesTest.java
+++ src/test/java/Carlos/OperacionesTest.java
@@ -5,14 +5,29 @@ import static org.junit.Assert.assertEquals;
 
 public class OperacionesTest {
     @Test
-    public void operationTestSum() {
+    public void givenTwoNumbersWhenSumThenResult() {
         // given
-        Operaciones operaciones = new Operaciones(3,3);
+        int firstValue = 3;
+        int secondValue = 3;
+        Operaciones operaciones = new Operaciones(firstValue,secondValue);
 
         // when
         String result = operaciones.sumar();
 
         // then
         assertEquals(result, "el resultado de la suma es: 6.0");
+    }
+    @Test
+    public void givenTwoNumbersWhenRestThenResult() {
+        // given
+        int firstValue = 12;
+        int secondValue = 3;
+        Operaciones operaciones = new Operaciones(firstValue,secondValue);
+
+        // when
+        String result = operaciones.restar();
+
+        // then
+        assertEquals(result, "el resultado de la resta es: 9.0");
     }
 }
